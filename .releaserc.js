11d33717158f35cc93035e625f78757e0de8a2e6
Steve Haar <info@stevehaar.com>
2022-01-23 12:55:25 -0600

    feat: add continuous integration

---
+++ .releaserc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'chore', release: false }
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      'semantic-release-npm-deprecate-old-versions',
+      {
+        rules: [
+          {
+            rule: 'supportLatest',
+            options: {
+              numberOfMajorReleases: 2,
+              numberOfMinorReleases: 1,
+              numberOfPatchReleases: 1
+            }
+          },
+          'deprecateAll'
+        ]
+      }
+    ],
+    'semantic-release-slack-bot'
+  ]
+};
